0bcd5fe9b93b6ca2e36e56575500899dac8916fb
Ubiagba Timothy <noir210397@gmail.com>
2026-02-25 09:11:10 +0000

    api test

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // const url = process.env.NEXT_PUBLIC_BASE_API_URL
 
-const instance = axios.create({ baseURL: `https://agencyapi-bwp7.onrender.com/api/job`, })
+const instance = axios.create({ baseURL: `https://agencyapi-bwp7.onrender.com/api/`, })
 // console.log(instance.defaults.baseURL);
 
 
